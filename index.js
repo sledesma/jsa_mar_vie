86f2d56170d906bc21b6d8c1b375b39b23e2dcbf
sledesma <sebas@Scorpion.sebas.com>
2021-04-23 11:19:46 -0300

    Clase 1 - Mitad

---
+++ index.js
@@ -0,0 +1,34 @@
+// 1. Elementos del Lenguaje (ECMAScript)
+// 2. Interfaces del Intérprete (APIs -Application Programming Interface-)
+/*
+ * Nombre:
+ * Casos de uso:
+ * F (funciones):
+ * E (eventos):
+ * O (objetos):
+ * (... que aporta al ámbito global)
+**/
+
+/*
+ * Nombre: API Console
+ * Casos de uso: Gestionar la consola de JavaScript
+ * F (funciones):
+ * E (eventos):
+ * O (objetos): console
+ * (... que aporta al ámbito global)
+**/
+console.table([
+  1, 2, 3, 4
+])
+
+console.log([
+  1, 2, 3, 4
+])
+
+console.warn("advertencia")
+
+console.error("Error")
+
+console.clear()
+
+console.assert(false, "Hola")
